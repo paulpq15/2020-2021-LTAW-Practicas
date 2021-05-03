b1c099c482b9faa45760a627a9a9f2dad1a24d16
paulpq15 <paul.0515@hotmail.com>
2021-05-04 00:47:38 +0200

    Completando Servidor

--- P1/Tienda/tienda.js
+++ P1/Tienda/tienda.js
@@ -6,6 +6,9 @@ const url = require('url');
 //-- Definir el puerto a utilizar
 const PORT = 9000;
 
+//-- Mensaje de arranque
+console.log("Arrancando servidor...");
+
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
     
@@ -34,9 +37,40 @@ const server = http.createServer((req, res) => {
   type_file = filename.split(".")[1] //--Se coge la extensión del archivo
   filename = "." + filename //--Para leer el fichero. Sin "." no nos funciona
 
+  console.log("Nombre del fichero: " + filename);
+  console.log("Tipo de fichero: " + type_file);
+
+  //-- Lectura del fichero (Asíncrona)
+  fs.readFile(filename, function(err, data) {
+  //-- Fichero no encontrado. Devolver mensaje de error
+    if ((err) || filename == "./error.html") {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        return res.end("200 OK")
+    }
+    //-- Tipo mime por defecto: html --> TIPO DE ARCHIVO
+    var mime = "text/html"
+    //Tipo de imágenes
+    if (type_file == 'png' || type_file == 'jpg') {
+      mime = "image/" + type_file;
+    }
+
+    // CSS
+    if (type_file == "css"){
+      mime = "text/css";
+    }
+
+  //-- Generar el mensaje de respuesta
+    res.write(data);
+    res.end();
+
+    });
+
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
-server.listen(PUERTO);
+server.listen(PORT);
 
-console.log("Servidor activado. Escuchando en puerto: " + PUERTO);
+console.log("Servidor activado. Escuchando en puerto: " + PORT);
