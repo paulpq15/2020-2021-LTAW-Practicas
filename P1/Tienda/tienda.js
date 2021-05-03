08a9834e9e6109400f7b8fe6bc570153d9cd12f5
paulpq15 <paul.0515@hotmail.com>
2021-05-03 23:46:56 +0200

    Seguimos

--- P1/Tienda/tienda.js
+++ P1/Tienda/tienda.js
@@ -4,11 +4,9 @@ const fs = require('fs');
 const url = require('url');
 
 //-- Definir el puerto a utilizar
-const PUERTO = 9000;
+const PORT = 9000;
 
-//-- Crear el servidor. La función de retrollamada de
-//-- atención a las peticiones se define dentro de los
-//-- argumentos
+//-- Crear el servidor
 const server = http.createServer((req, res) => {
     
   //-- Indicamos que se ha recibido una petición
@@ -18,6 +16,24 @@ const server = http.createServer((req, res) => {
   //-- Y escribir en consola la ruta de nuestro recurso
   const myURL = new URL(req.url, 'http://' + req.headers['host']);
   console.log("  Ruta del recurso: " + myURL.pathname);
+
+  //-- Según el pathname, se define el fichero
+  //-- con el que se responderá la petición
+
+  // Definimos la variable fichero
+  var filename = "";
+
+  // Fichero que obtendremos
+  if (myURL.pathname == "/") {
+    filename += "/index.html"; //petición de la pag principal 
+  } else {
+    filename += myURL.pathname; //petición de cualquier otro recurso
+  }
+  
+  //-- Para sacar el tipo de fichero
+  type_file = filename.split(".")[1] //--Se coge la extensión del archivo
+  filename = "." + filename //--Para leer el fichero. Sin "." no nos funciona
+
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
