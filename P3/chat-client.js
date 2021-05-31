c22c8820225c363cf4562f193d7a70bc060efe9f
paulpq15 <paul.0515@hotmail.com>
2021-05-31 18:31:57 +0200

    Añadiendo la mejora de "usuario x escribiendo"

--- P3/chat-client.js
+++ P3/chat-client.js
@@ -12,19 +12,36 @@ let tono = new Audio('tonomensaje.mp3');
 //Creamos variable para el nick 
 let nickname = 'Anónimo';
 
+//-- Variable que muestra si se escribe
+let escribiendo = false;
+
 socket.on("message", (msg)=>{
   display.innerHTML += '<p style="color:black">' + msg + '</p>';
-  tono.play();
+  if(!msg.includes('esta escribiendo...')){
+    tono.play();
+  }
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
-  if (msg_entry.value)
+  if (msg_entry.value){
     socket.send(nickname + ': ' + msg_entry.value);
-  
+    escribiendo = false;
+  }
   //-- Borrar el mensaje actual
   msg_entry.value = "";
 }
+
+//-- Al estar escribiendo se les manda un mensaje a los usuarios
+msg_entry.oninput = () => {
+  //-- Si esta escribiendo
+  if(!escribiendo){
+    escribiendo = true;
+    socket.send('El usuario ' + nickname + ' esta escribiendo...');
+  };
+};
+
+console.log(nickname)
 
 msg_nick.onchange = () => {
   if (msg_nick.value){
