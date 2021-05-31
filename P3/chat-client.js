4848ab2345a3704491c9a188316a51a5d8bc7681
paulpq15 <paul.0515@hotmail.com>
2021-05-31 20:11:37 +0200

    Modificando cosas que no me funcionaban en el navegadrr

--- P3/chat-client.js
+++ P3/chat-client.js
@@ -10,42 +10,24 @@ const socket = io();
 let tono = new Audio('tonomensaje.mp3');
 
 //Creamos variable para el nick 
-let nickname = 'Anónimo';
-
-//-- Variable que muestra si se escribe
-let escribiendo = false;
+let nickname = 'Desconocido';
 
 socket.on("message", (msg)=>{
-  display.innerHTML += '<p style="color:black">' + msg + '</p>';
-  if(!msg.includes('esta escribiendo...')){
-    tono.play();
-  }
+  display.innerHTML += '<p style="color:blue">' + msg + '</p>';
+  tono.play();
 });
 
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
   if (msg_entry.value){
     socket.send(nickname + ': ' + msg_entry.value);
-    escribiendo = false;
   }
   //-- Borrar el mensaje actual
   msg_entry.value = "";
-}
-
-//-- Al estar escribiendo se les manda un mensaje a los usuarios
-msg_entry.oninput = () => {
-  //-- Si esta escribiendo
-  if(!escribiendo){
-    escribiendo = true;
-    socket.send('El usuario ' + nickname + ' esta escribiendo...');
-  };
 };
-
-console.log(nickname)
 
 msg_nick.onchange = () => {
   if (msg_nick.value){
-    nick = msg_nick.value;
+    nickname = msg_nick.value;
   }
-  console.log(nickname);
 };
