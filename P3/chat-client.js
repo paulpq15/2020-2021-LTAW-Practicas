c3c874ca34f80d024102cf5022e732ededb86d3f
paulpq15 <paul.0515@hotmail.com>
2021-05-31 18:02:30 +0200

    Añadiendo otra mejora esta es la del nick

--- P3/chat-client.js
+++ P3/chat-client.js
@@ -1,12 +1,16 @@
 //-- Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
+const msg_nick = document.getElementById("msg_nick");
 
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
 //-- Cargar sonido para cuando se reciba un mensaje
 let tono = new Audio('tonomensaje.mp3');
+
+//Creamos variable para el nick 
+let nickname = 'Anónimo';
 
 socket.on("message", (msg)=>{
   display.innerHTML += '<p style="color:black">' + msg + '</p>';
@@ -16,8 +20,15 @@ socket.on("message", (msg)=>{
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
   if (msg_entry.value)
-    socket.send(msg_entry.value);
+    socket.send(nickname + ': ' + msg_entry.value);
   
   //-- Borrar el mensaje actual
   msg_entry.value = "";
 }
+
+msg_nick.onchange = () => {
+  if (msg_nick.value){
+    nick = msg_nick.value;
+  }
+  console.log(nickname);
+};
